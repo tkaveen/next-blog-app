9a7ea18035979b0857577962a4f2bb02dc7d2a4e
Theshan Kaveen <kaveenliyanaarachchi@gmail.com>
2024-12-19 21:22:24 +0530

    layout added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,18 @@
+import Featured from "@/components/featured/Featured";
 import styles from "./homepage.module.css";
+import CategoryList from "@/components/categoryList/Navbar";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div>
   );
 }
